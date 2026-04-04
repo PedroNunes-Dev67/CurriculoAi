0a0cb9ca5471501ec3956d9a6505562d5eb5147f
tayssobrt <gui.andrevieira@gmail.com>
2026-04-03 22:41:27 -0300

    refractor: remove display do cadastro

--- curriculo-ai-frontend/src/app/(tabs)/_layout.tsx
+++ curriculo-ai-frontend/src/app/(tabs)/_layout.tsx
@@ -2,9 +2,11 @@ import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="cadastro" />
-      <Tabs.Screen name="formacao" />
+    <Tabs screenOptions={{ headerShown: false}}>
+      <Tabs.Screen
+    name="cadastro"
+    options={{ tabBarStyle: { display: "none" } }}
+    />
     </Tabs>
   );
 }
