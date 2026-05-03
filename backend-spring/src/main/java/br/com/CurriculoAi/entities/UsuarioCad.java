514d1903d433850fbc6b88e38870e4af481b33f6
tayssobrt <gui.andrevieira@gmail.com>
2026-05-03 16:09:09 -0300

    Feat(entities) add UsuarioCad and AreaUser entities with ManyToOne relationship

---
+++ backend-spring/src/main/java/br/com/CurriculoAi/entities/UsuarioCad.java
@@ -0,0 +1,32 @@
+package br.com.CurriculoAi.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "usuario_cad")
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioCad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome", nullable = false, length = 80)
+    private String nome;
+
+    @Column(name = "email", nullable = false, length = 50)
+    private String email;
+
+    @Column(name = "senha", nullable = false, length = 30)
+    private String senha;
+
+    @ManyToOne
+    @JoinColumn(name = "id_area", nullable = false)
+    private AreaUser areaUser;
+}
